99ce5805d99b3976cf54fccd2bf1fac3fa0c22e5
daniel <kedi_dn@hotmail.com>
2021-09-29 14:20:30 +0200

    First commit

---
+++ projet_pizza/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projet_pizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
